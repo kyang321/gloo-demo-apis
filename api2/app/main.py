8ef2567dc67bf2bece487f217ce41ccbc04c7daf
Kenny Yang <kyang@pop-os.localdomain>
2019-07-01 01:42:05 -0400

    Initial Commit. Docker-compose version of the test apis

---
+++ api2/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {
+            "item_id": item_id,
+            "api2:": "this came from api2"
+            }
